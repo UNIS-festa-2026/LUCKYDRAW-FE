4e5b66c9cc06a68f90ffd1c683982370cd30d399
1eehvunzin <hyunjinlee@ewha.ac.kr>
2026-05-18 23:16:52 +0900

    fix: 빌드 오류 해결

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -118,7 +118,6 @@ export default function Home() {
                 <ReviewCard
                   key={review.id}
                   name={review.name}
-                  reward={review.reward}
                   comment={review.comment}
                   variant={review.variant}
                 />
